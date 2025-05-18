29e7853f4973a96ad75cedb3782d0f9ba7141bd2
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-05-18 15:52:40 +0200

    Vending Machine Interaction Script

---
+++ Assets/VendingMachineInteraction.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class VendingMachineInteraction : MonoBehaviour
+{
+    public Image progressBar;
+    public AudioSource vendingAudio;
+    public UnityEvent onFinishedEating; // Player gains energy
+
+    private int clickCount = 0;
+    private int requiredClicks = 8;
+
+    private void OnMouseDown()
+    {
+        clickCount++;
+
+        if (vendingAudio != null )
+            vendingAudio.Play();
+
+        UpdateProgressBar();
+
+        if (clickCount >= requiredClicks)
+        {
+            onFinishedEating?.Invoke();
+            clickCount = 0; //Reset
+            UpdateProgressBar();
+        }
+    }
+
+    void UpdateProgressBar()
+    {
+        if(progressBar != null)
+            progressBar.fillAmount = (float)clickCount / requiredClicks;
+    }
+}
