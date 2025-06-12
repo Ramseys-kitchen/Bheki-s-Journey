3ea341fd69b0a1d46c495ab8da2f24a87ace48ba
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-06-12 21:43:44 +0200

    editing moved all these scripts to new UI
    These scripts were under an old UI in the hierarchy and placed under a canvas under ground so that its all in one place

--- Assets/Scripts/Slot.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class Slot : MonoBehaviour
-{
-    public GameObject currentItem; //The item currently held in this slot
-}
