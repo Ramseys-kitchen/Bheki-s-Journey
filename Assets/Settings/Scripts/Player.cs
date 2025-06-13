79cad723608f22cf35bd1c5da939e00ae717d81b
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-06-13 22:23:21 +0200

    Updated player script

--- Assets/Settings/Scripts/Player.cs
+++ Assets/Settings/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class PlayerCtrl : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     
