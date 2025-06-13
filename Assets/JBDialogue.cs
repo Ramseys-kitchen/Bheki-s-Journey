c9a8e252141138e020d0ab0a1f6fd64332c24998
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-06-13 23:39:13 +0200

    Updated John Beers Dialogue
    Edited spelling errors

--- Assets/JBDialogue.cs
+++ Assets/JBDialogue.cs
@@ -21,28 +21,28 @@ public class JBDialogue : MonoBehaviour
         hasTalked = true;
         dialogueUI.SetActive(true);
 
-        dialogueText.text = "John Beers: JA! cyril neh I need you to..";
-        yield return new WaitForSeconds(4f);
+        dialogueText.text = "John Beers: JA! Sibusiso neh I need you to..";
+        yield return new WaitForSeconds(5f);
 
         dialogueText.text = "Bheki: Bheki sir...it's Bheki";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "John Beers: Bheki....right..Bheki Mmmm";
+        dialogueText.text = "John Beers: Bheki....right....Bheki Mmmm";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Bheki: Uhmm when will I be assigned a desk. I've been here a week";
+        dialogueText.text = "Bheki: Uhmm when will I be assigned a desk. I've been here a week now";
         yield return new WaitForSeconds(5f);
 
         dialogueText.text = "John Beers: Your desk? relax neh its coming. Ag jy weet moes (you know how it is)";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "John Beers: Since you here just clean up this mess for me its useless paperwork";
+        dialogueText.text = "John Beers: Since you here already just clean up this mess for me its useless paperwork";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "John Beers: (Yoh so I really left home to be a white oaks cleaner)";
+        dialogueText.text = "Bheki: (*Hmm so I really left home to be a white oaks cleaner pfff*)";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Bheki: (Maybe I'll find those things Cyril told me about, bring this company down and get a fat paycheck)";
+        dialogueText.text = "Bheki: (*Maybe I'll find those things Sibusiso told me about, bring this company down,  and get a fat paycheck*)";
         yield return new WaitForSeconds(5f);
 
         dialogueText.text = "Game: (Pick up the objects you see by walking over them)";
