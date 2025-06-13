8886e68461910bae49ec0adb7c557e3ff66385f2
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-06-13 23:37:14 +0200

    Updated NPC Dialogue script

--- Assets/Settings/Scripts/NPCDialogue.cs
+++ Assets/Settings/Scripts/NPCDialogue.cs
@@ -21,31 +21,51 @@ public class NPCDialogue : MonoBehaviour
         hasTalked = true;
         dialogueUI.SetActive(true);
 
-        dialogueText.text = "Cyril: Hi youre Bheki I heard youre the new cleaner..uhmm office hire I mean. Welcome to John Beers finance";
-        yield return new WaitForSeconds(4f);
-
-        dialogueText.text = "Bheki: Uhmmm? Yebo bhuthi (Yes brother) that's me";
+        dialogueText.text = "Sibusiso: Hi you're Bheki I heard you're the new cleaner..uhmm office hire I mean. Welcome to John Beers finance.";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Cyril: Mfethu (my brother) I dont know what they told you but no one really cares about you here. You're just a cleaner in their eyes";
+        dialogueText.text = "Bheki: Uhmmm? Yes that's me.";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Bheki: Uhmmm askies bhuthi? (sorry brother?) what do mean....I applied for an office job.";
+        dialogueText.text = "Sibusiso: Mfethu (my brother) I dont know what they told you but no one really cares about you here. You're just a cleaner in their eyes.";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Cyril: That means nothing these oaks dont respect you, not even that fancy clipboard you have.";
+        dialogueText.text = "Bheki: A cleaner? uhmmm askies bhuthi? (sorry brother?) what do mean....I applied for an office job you might have the wrong Bheki.";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Bheki: Is this why I haven't been assigned a desk, I'm spending my whole day picking up rubbish!? ";
+        dialogueText.text = "Sibusio: Hey listen that means nothing here these oaks dont respect you, not that suit, tie, or even that fancy clipboard you have.";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Cyril : Exactly, So ungazenzi smart (Dont act too clever) especially in front of that John Beers I know something about him, just continue cleaning up this office";
+        dialogueText.text = "Bheki: Is this why I haven't been assigned a desk, I'm spending my whole day picking up rubbish.!? ";
         yield return new WaitForSeconds(5f);
 
-        dialogueText.text = "Bheki: John Beers... Grootman? (The boss?), what do you know about him";
+        dialogueText.text = "Sibusio: Exactly that, So ungazenzi smart (Dont act too clever) especially in front of that John Beers I know something about him, just continue cleaning up this office.";
         yield return new WaitForSeconds(5f);
 
-       
+        dialogueText.text = "Bheki: John Beers... Grootman? (The boss?), what do you know about him.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Sibusio: Ey man I'm gonna put you onto to something (glances around). You know Johns office. That place is always a mess.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Sibusio: But deep inside that mess there's files, I'm talking real stuff, HR- level type of complaints.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Bheki: Say you lie.?!";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Sibusiso: Mistreatment and dodgy contracts about how they hire black okes like us to balance the numbers but we just end up pushing brooms.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Sibusio: But listen if you get those files you could get a payout. A hush money type of thing.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Sibusio: We split that money, you can decide the cut. I just wanna get out of here with something.";
+        yield return new WaitForSeconds(5f);
+
+        dialogueText.text = "Game: (Walk up to John Beers Office)";
+        yield return new WaitForSeconds(5f);
+
         dialogueUI.SetActive(false);
     }
 }
